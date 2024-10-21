7264e91edadb55ee1bf1318b2ccdf3cc6e2eb660
cricci3 <100707767+cricci3@users.noreply.github.com>
2024-10-21 20:10:44 +0200

    Exercise 3

---
+++ DL-exercize-3/prova.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+
+import torch
+from torch import nn
+import torch.optim as optim
+
+print('namo')
